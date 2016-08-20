3e1b777320ddd0d6aa6c31d9e7b9f25985b98542
Zhenjiang Liu <zliu@kpd-i.com>
2016-08-20 13:22:45 -0400

    Initial Commit

---
+++ HWS-2-demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HWS-2-demo
+//
+//  Created by ZhongZhongzhong on 2016-08-20.
+//  Copyright © 2016 ZhongZhongzhong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
